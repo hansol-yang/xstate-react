2ba57a316653c4391dfd71324dfb6742ccc9ef84
hansolyang9 <hansolyan9@gmail.com>
2020-11-16 01:08:26 +0900

    create mocked function for next/router

--- components/Navbar/Navbar.test.tsx
+++ components/Navbar/Navbar.test.tsx
@@ -1,15 +1,24 @@
+import { useRouter } from 'next/router';
 import { Navbar } from './';
 
 import { fireEvent, render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
-const useRouter = jest.mock('next/router', () => {
-    return {
-        push: jest.fn()
-    };
-})
+jest.mock('next/router');
 
 describe('Navbar', () => {
+    let pathname = '/';
+    const mockedRouter = useRouter as jest.Mock;
+    const mockedPush = jest.fn();
+    mockedPush.mockImplementationOnce((uri: string) => {
+        pathname = uri;
+        Promise.resolve(true);
+    });
+    mockedRouter.mockImplementation(() => ({
+        pathname,
+        push: mockedPush,
+    }));
+
     test('renders login anchor', () => {
         const { getByRole } = render(<Navbar />);
 
@@ -19,7 +28,9 @@ describe('Navbar', () => {
     test('route to "/auth/login", if click login anchor', () => {
         const { container, getByText } = render(<Navbar />);
 
-        
         fireEvent.click(getByText('Login'));
+
+        expect(mockedPush).toHaveBeenCalledTimes(1);
+        expect(useRouter().pathname).toBe('/auth/login');
     });
 });
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,18 +1,19 @@
-import { useRouter } from 'next/dist/client/router';
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export function Navbar() {
     const router = useRouter();
     const _onClickLogin = (e: React.MouseEvent) => {
         e.preventDefault();
-        router.push('/auth/login')
-    }
+        router.push('/auth/login');
+    };
     return (
         <nav>
             {/* <Link href='/auth/login'>
                 <a>Login</a>
             </Link> */}
-            <a href="#" onClick={_onClickLogin}>Login</a>
+            <a href='#' onClick={_onClickLogin}>
+                Login
+            </a>
         </nav>
     );
 }
